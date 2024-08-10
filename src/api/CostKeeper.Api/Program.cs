4eab7186d4d9e126ef982251842b06396dded7d8
Alex <AlexKozlovDeveloper@gmail.com>
2024-08-11 01:32:37 +0300

    Add Cors

--- src/api/CostKeeper.Api/Program.cs
+++ src/api/CostKeeper.Api/Program.cs
@@ -21,6 +21,17 @@ builder.Services.AddDbContext<CostsDbContext>(
 
 builder.Services.AddMediatR(cfg => { cfg.RegisterServicesFromAssembly(typeof(Program).Assembly); });
 
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(
+	"AllowAll",
+	policy =>
+	{
+		policy.AllowAnyOrigin()
+			  .AllowAnyMethod()
+			  .AllowAnyHeader();
+	});
+});
 
 var app = builder.Build();
 
@@ -32,6 +43,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
