5838f6b7be4f7fc574e514e9d5e15a8d71c3b7ab
Alex <AlexKozlovDeveloper@gmail.com>
2024-08-12 00:39:43 +0300

    Update seed data

--- src/api/CostKeeper.Core/Models/Product.cs
+++ src/api/CostKeeper.Core/Models/Product.cs
@@ -3,11 +3,13 @@
     public class Product
     {
         public string Id { get; set; }
-
         public string? Description { get; set; }
         public string Category { get; set; }
         public string[] Tags { get; set; }
-        public int Weight { get; set; }
-        public int CaloriesPer100g { get; set; }        
-    }
+        public float? Weight { get; set; }
+        public float? EnergyValue { get; set; }        
+        public float? Proteins { get; set; }        
+        public float? Fats { get; set; }        
+        public float? Carbohydrates { get; set; }
+	}
 }
