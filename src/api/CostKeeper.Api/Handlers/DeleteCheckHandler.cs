b59f9d1103a9a02d8ae3d0e9a4ae0601c4daafe5
Alex <AlexKozlovDeveloper@gmail.com>
2024-08-17 14:41:06 +0300

    Create DeleteCheckHandler.cs

---
+++ src/api/CostKeeper.Api/Handlers/DeleteCheckHandler.cs
@@ -0,0 +1,44 @@
+﻿using CostKeeper.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostKeeper.Api.Handlers
+{
+	[ApiController]
+	[Route("v1/checks")]
+	public class DeleteCheckController(IMediator mediator) : ControllerBase
+	{
+		private readonly IMediator _mediator = mediator;
+
+		[HttpDelete("{checkId}")]
+		public async Task<ActionResult<IEnumerable<Check>>> Handle(
+			[FromRoute] int checkId
+			)
+		{
+			var items = await _mediator.Send(new DeleteCheckCommand(checkId));
+			return Ok(items);
+		}
+	}
+
+	public record DeleteCheckCommand(
+		int CheckId
+		) : IRequest<Check>;
+
+	public class DeleteCheckHandler(CostsDbContext dbContext) : IRequestHandler<DeleteCheckCommand, Check>
+	{
+		private readonly CostsDbContext _dbContext = dbContext;
+
+		public async Task<Check> Handle(DeleteCheckCommand request, CancellationToken ct)
+		{
+			var check = await _dbContext.Checks
+				.FirstAsync(a => a.Id == request.CheckId, ct);
+
+			_dbContext.Checks.Remove(check);
+
+			await _dbContext.SaveChangesAsync(ct);
+
+			return check;
+		}
+	}
+}
